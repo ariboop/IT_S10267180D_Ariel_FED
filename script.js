a1d1876e95e08c65651d7abcde083ff0c26ec010
De Roza Ariel Therese <s10267180@connect.np.edu.sg>
2024-12-01 23:36:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+function visualin(dif){
+    var vis;
+    if (dif==0) {
+        vis="Images/visualin.jpg";
+    } else if (dif==2){
+        vis="Images/visualin1.jpg";
+    } else if (dif==3){
+        vis="Images/visualin2.jpg";
+    }
+    document.getElementById("Visual").src = vis;
+}
+
+function visualf(dif){
+    var vis;
+    if (dif==0) {
+        vis="Images/visualf.jpg";
+    } else if (dif==2){
+        vis="Images/visualf1.jpg";
+    } else if (dif==3){
+        vis="Images/visualf2.jpg";
+    }
+    document.getElementById("Visual").src = vis;
+}
+
+function visualsm(dif){
+    var vis;
+    if (dif==0) {
+        vis="Images/SMFASHION.webp";
+    } else if (dif==2){
+        vis="Images/SMFASHION1.jpg";
+    } else if (dif==3){
+        vis="Images/SMFASHION2.jpg";
+    }
+    document.getElementById("Visual").src = vis;
+}
+
+function visualh(dif){
+    var vis;
+    if (dif==0) {
+        vis="Images/danceh.jpg";
+    } else if (dif==2){
+        vis="Images/danceh1.jpg";
+    } else if (dif==3){
+        vis="Images/danceh2.jpg";
+    }
+    document.getElementById("Visual").src = vis;
+}
+
+function visualhj(dif){
+    var vis;
+    if (dif==0) {
+        vis="Images/visualhj.jpg";
+    } else if (dif==2){
+        vis="Images/visualhj1.jpg";
+    } else if (dif==3){
+        vis="Images/visualhj2.jpg";
+    }
+    document.getElementById("Visual").src = vis;
+}
+
+function visualcb(dif){
+    var vis;
+    if (dif==0) {
+        vis="Images/artistcb.jpg";
+    } else if (dif==2){
+        vis="Images/artistcb1.jpg";
+    } else if (dif==3){
+        vis="Images/artistcb2.jpg";
+    }
+    document.getElementById("Visual").src = vis;
+}
+
+function visuallk(dif){
+    var vis;
+    if (dif==0) {
+        vis="Images/visuallk.jpg";
+    } else if (dif==2){
+        vis="Images/visuallk1.jpg";
+    } else if (dif==3){
+        vis="Images/visuallk2.jpg";
+    }
+    document.getElementById("Visual").src = vis;
+}
+
+function visualchan(dif){
+    var vis;
+    if (dif==0) {
+        vis="Images/visualc.webp";
+    } else if (dif==2){
+        vis="Images/visualc1.jpg";
+    } else if (dif==3){
+        vis="Images/visualc2.jpg";
+    }
+    document.getElementById("Visual").src = vis;
+}
